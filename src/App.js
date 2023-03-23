233dba8a98e55e598c89ddac3dbf4e73a985c0da
Thiago Santos <thiago_santos_1990@hotmail.com>
2023-03-23 12:06:42 -0300

    primeiro programa em react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Button from "./Components/Button";
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+      
+      <Button title = "Login" />
+      <Button title = "Logout" />
+
+    </div>
+  );
+}
+
+export default App;
